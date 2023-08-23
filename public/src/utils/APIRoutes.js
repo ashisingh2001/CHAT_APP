cfd310721d9a2d0a75a7a016bd9083269d6c1954
Ashi Singh <68825482+ashisingh2001@users.noreply.github.com>
2023-08-23 16:33:27 +0530

    Update APIRoutes.js

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://chat-app-backend-git-master-ashisingh2001.vercel.app";
+export const host = "https://chat-app-backend-7ieaxaz3m-ashisingh2001.vercel.app";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setavatar`;
