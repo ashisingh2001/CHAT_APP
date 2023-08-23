928d2e159cd69d5c86995a0701d37c08443876fa
Ashi Singh <68825482+ashisingh2001@users.noreply.github.com>
2023-08-23 11:49:42 +0530

    Update APIRoutes.js

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://chat-app-backend-mauve.vercel.app/";
+export const host = "https://chat-app-iota-tan.vercel.app/";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setavatar`;
